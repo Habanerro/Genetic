7e7d15456e51483dd45edb6bcf242089a4f6be45
Иван <fitohal@yandex.ru>
2018-09-22 18:44:50 +0700

    Add Important file header_place.h

---
+++ code/place/header_place.h
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+using namespace std;
+
+typedef unsigned char uchar;
+
+// start settings of place
+struct SettingsPlace {
+	uchar lenght;	// quantity of cols - длина
+	uchar height;	// quantity of rows - высота
+	uchar bot;		// quantity of bots		'O'()
+	uchar food;		// quantity of food 	'h'()
+	uchar poison;	// quantity of poison 	'p'()
+	uchar wall; 	// quantity of wall's 	'X'()
+	string settings;// name of file of the settings
+};
+typedef struct SettingsPlace sPlace;
+
+// information about place in real time
+class Place {
+	friend: main();
+public:
+	void generate_place(); // to create basis of place
+
+	Place(sPlace);	// normal constructor
+	~Place();		// destructor
+protected:
+	uchar all;		// quantity of all cell points
+	uchar bot;		// quantity of bots
+	uchar food;		// quantity of food points
+	uchar poison;	// quantity of poison points
+	uchar wall;		// quantity of wall points
+
+	char** p;		// pointer to place
+	string settings;// name of file of the settings
+};
+typedef class Place cPlace;
