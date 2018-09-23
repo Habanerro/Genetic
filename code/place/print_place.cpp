bee691e597d8819297cb0921ef3f6b3cdae75568
Иван <fitohal@yandex.ru>
2018-09-23 21:29:30 +0700

    NOT FIX color output

--- code/place/print_place.cpp
+++ code/place/print_place.cpp
@@ -2,34 +2,30 @@
 
 void Place::print_place()
 {
-	log << "\nTo print" << endl;
+	log << "\tTo print" << endl;
 		int i = 0, j = 0;
 		for (i = 0; i < height; i++) {
 			cout << "\n " << endl;
 			for (j = 0; j < lenght; j++) {
-				/*switch (ptr[i][j]) {
-					case '_':	// free geks
-						printf("\033[37m %c\033[0m", ptr[i][j]);
+				switch (ptr[i][j]) {
+					case 'X':
+						printf("  %c", ptr[i][j]);
 						break;
-
-					case 'X':	// wall
-						printf("\033[1m %c\033[0m", ptr[i][j]);
+					case '_':
+						printf("  %c", ptr[i][j]);
 						break;
-
-					case 'O':	// bot
-						printf("\033[36m %c\033[0m", ptr[i][j]);
+					case 'O':
+						printf("  %c", ptr[i][j]);
 						break;
-
-					case 'f':	// food
-						printf("\033[32m %c\033[0m", ptr[i][j]);
+					case 'f':
+						printf("  %c", ptr[i][j]);
 						break;
-
-					case 'p':	// poison
-						printf("\033[31m %c\033[0m", ptr[i][j]);
+					case 'p':
+						printf("  %c", ptr[i][j]);
 						break;
-				} NOT WORKED*/
-				printf(" %c", ptr[i][j]); 
+				}
 			}
 		}
-	log << "End print" << endl;
+		cout << endl;
+	log << "\tEnd print" << endl;
 }
