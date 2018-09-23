9a1113caa2e517633581043d090df7c149feec96
Иван <fitohal@yandex.ru>
2018-09-23 21:44:35 +0700

    FIX coclor's problem

--- code/place/print_place.cpp
+++ code/place/print_place.cpp
@@ -3,29 +3,34 @@
 void Place::print_place()
 {
 	log << "\tTo print" << endl;
+	
+	system("chcp 65001");
+	system("cls");
+		log << "\t\t Clear console + encode Unicode" << endl;
 		int i = 0, j = 0;
 		for (i = 0; i < height; i++) {
 			cout << "\n " << endl;
 			for (j = 0; j < lenght; j++) {
 				switch (ptr[i][j]) {
 					case 'X':
-						printf("  %c", ptr[i][j]);
+						printf("\033[34m  %c\033[0m", ptr[i][j]);
 						break;
 					case '_':
-						printf("  %c", ptr[i][j]);
+						printf("\033[2m  %c\033[0m", ptr[i][j]);
 						break;
 					case 'O':
-						printf("  %c", ptr[i][j]);
+						printf("\033[34m  %c\033[0m", ptr[i][j]);
 						break;
 					case 'f':
-						printf("  %c", ptr[i][j]);
+						printf("\033[32m  %c\033[0m", ptr[i][j]);
 						break;
 					case 'p':
-						printf("  %c", ptr[i][j]);
+						printf("\033[31m  %c\033[0m", ptr[i][j]);
 						break;
 				}
 			}
 		}
 		cout << endl;
+
 	log << "\tEnd print" << endl;
 }
