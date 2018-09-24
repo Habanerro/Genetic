906ca1f5b25c00c4d53282cac9e126990d52d8e6
Иван <fitohal@yandex.ru>
2018-09-24 18:24:47 +0700

    Carcas of the class Bot

--- code/bot/header_bot.h
+++ code/bot/header_bot.h
@@ -5,4 +5,24 @@
 #include <cstdio>
 #include <cmath>
 #include <ctime>
+
+#include "../place/header_place.h"
 using namespace std;
+
+class Bot {
+	friend main();
+public:
+
+	Bot();	// constructor
+	~Bot();	// destructor
+protected:
+	unint gquant;	// quantity of gens in genetic matrix
+	unint grn;		// gen right now - pointer to gen in genetic matrix
+	unint hp;		// healths points
+	unint lp;		// live points - all hp in the bot's live
+	unint ya;		// index of this bot
+	unint next;		// index of the next undead bot in the
+
+	char* gen;		// genetic matrix
+	ofstream log;	// information about working
+};
