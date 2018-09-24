414aa8abf5a46209326ed014e05d4247cde54f55
Иван <fitohal@yandex.ru>
2018-09-24 15:10:18 +0700

    FIX outside in spawn-function

--- code/place/spawn_poison.cpp
+++ code/place/spawn_poison.cpp
@@ -9,21 +9,42 @@ void Place::spawn_poison()
 		int i = 0, j = 0, g = 0, v = 0;
 		unint nHeight = height - 2;
 		unint nLenght = lenght - 2;
+
+		// we divide place on four fourth
+		unint lmid = nLenght / 2;
+		unint hmid = nHeight / 2;
+		unint fourth = -1;
+
 		log << "\t\t\tnHeight = " << nHeight
 			<< "; nLenght = " << nLenght << endl;
+		log << "\t\t\tHeight_mid = " << hmid
+			<< "; Lenght_mid = " << lmid << endl;
 
 		for (g = 0; g < poison; g++) {
 			i = 1 + rand() % nHeight;
 			j = 1 + rand() % nLenght;
+			if (i > hmid) {
+				if (j > lmid) {fourth = 4;} else {fourth = 3;}
+			} else {
+				if (j > lmid) {fourth = 1;} else {fourth = 2;}
+			}
 			//log << "\t\t\ti = " << i
 			//	<< "; j = " << j << endl;
 			while (ptr[i][j] != '_') {
-				v = rand() % 4;
-				switch (v) {
-					case 0: i++; break;
-					case 1: i--; break;
-					case 2: j++; break;
-					case 3: j--; break;
+				v = rand() % 2;
+				switch (fourth) {
+					case 1:
+						if (v) {i++;} else {j--;}
+						break;
+					case 2:
+						if (v) {i++;} else {j++;}
+						break;
+					case 3:
+						if (v) {i--;} else {j++;}
+						break;
+					case 4:
+						if (v) {i--;} else {j--;}
+						break;
 				}
 			}
 			ptr[i][j] = 'p';
