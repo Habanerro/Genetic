32d9e1f047677bcdf389c947facfd6938b94f893
Иван <fitohal@yandex.ru>
2018-10-03 18:30:45 +0700

    FIX overlay by wall's + OFF log.txt

--- code/place/spawn_wall.cpp
+++ code/place/spawn_wall.cpp
@@ -6,44 +6,8 @@ void Place::spawn_wall()
 	srand( time(NULL) );
 
 	log << "\t\tStaring generation..." << endl;
-	int i = 0, j = 0, g = 0, v = 0;
-		
-	// lenght >= height
-	int* xc = new int[all];
-	int* yc = new int[all];
-	int* uu = new int[all]; // used coordinates
 
-	for (i = 0; i < all; i++) {
-		uu[i] = 0;
-	}
-
-	for (i = 1; i < height - 1; i++) {
-		for (j = 1; j < lenght - 1; j++) {
-			if (ptr[i][j] == '_') {
-				xc[g] = i;
-				yc[g] = j;
-				g++;
-			}
-		}
-	}
-	log << "\t\tQuantity of free gekses: " << g << endl;
-
-	for (i = 0; i < wall; i++) {
-		v = rand() % g;
-		while (uu[v]) {
-			v = rand() % g;
-		}
-		
-		ptr[xc[v]][yc[v]] = 'X';
-		uu[v] = 1;
-	}
-	delete xc;
-	delete yc;
-	delete uu;
-/*
 	char direction = 0;	// four value's
-	char delta = 0;		// two value's
-	char chance = 0;	// three value's
 	char q = 5;			// quantity of wall's section in the wall segment
 
 	unint segments = wall / 5; // quantity of wall's segments
@@ -77,23 +41,10 @@ void Place::spawn_wall()
 				case 0: // up
 					a = i;
 					for (g = 0; g < q; g++) {
-						delta = rand() % 2;
-						chance = rand() % 3;
-						m = 0;
-						if (chance == 1) {
-							if (delta = 0) {
-								// to left
-								m = 1;
-							} else {
-								// to right
-								m = -1;
-							}
-						}
-
-						if (ptr[a][j + m] == 'X') {
+						if (ptr[a][j] == 'X') {
 							v++;
 						} else {
-							ptr[a][j + m] = 'X';
+							ptr[a][j] = 'X';
 						}
 						a++;
 					}
@@ -101,23 +52,10 @@ void Place::spawn_wall()
 				case 1:	// down
 					a = i;
 					for (g = 0; g < q; g++) {
-						delta = rand() % 2;
-						chance = rand() % 3;
-						m = 0;
-						if (chance == 1) {
-							if (delta = 0) {
-								// to left
-								m = 1;
-							} else {
-								// to right
-								m = -1;
-							}
-						}
-
-						if (ptr[a][j + m] == 'X') {
+						if (ptr[a][j] == 'X') {
 							v++;
 						} else {
-							ptr[a][j + m] = 'X';
+							ptr[a][j] = 'X';
 						}
 						a--;
 					}
@@ -125,46 +63,21 @@ void Place::spawn_wall()
 				case 2:	// right
 					a = j;
 					for (g = 0; g < q; g++) {
-						delta = rand() % 2;
-						chance = rand() % 3;
-						m = 0;
-						if (chance == 1) {
-							if (delta = 0) {
-								// to left
-								m = 1;
-							} else {
-								// to right
-								m = -1;
-							}
-						}
-
-						if (ptr[i + m][a] == 'X') {
+						if (ptr[i][a] == 'X') {
 							v++;
 						} else {
-							ptr[i + m][a] = 'X';
+							ptr[i][a] = 'X';
 						}
 						a++;
 					}
 					break;
 				case 3:	// left
 					a = j;
-					for (g = 0; g < q; g++) {
-						delta = rand() % 2;
-						chance = rand() % 3;
-						m = 0;
-						if (chance == 1) {
-							if (delta = 0) {
-								// to left
-								m = 1;
-							} else {
-								// to right
-								m = -1;
-							}
-						}
-						if (ptr[i + m][a] == 'X') {
+					for (g = 0; g < q; g++) {						
+						if (ptr[i][a] == 'X') {
 							v++;
 						} else {
-							ptr[i + m][a] = 'X';
+							ptr[i][a] = 'X';
 						}
 						a--;
 					}
@@ -184,14 +97,15 @@ void Place::spawn_wall()
 	}
 	log << "\t\tNeed save " << v << " walls" << endl;
 	for (i = 0; i < v; i++) {
-		v = rand() % g;
-		while (uu[v]) {
-			v = rand() % g;
+		m = rand() % g;
+		while (uu[m]) {
+			m = rand() % g;
 		}
 			
-		ptr[xc[v]][yc[v]] = 'X';
-		uu[v] = 1;
-	}*/
+		ptr[xc[m]][yc[m]] = 'X';
+		uu[m] = 1;
+	}
+
 	log << "\t\t\tEnd of generation." << endl;
 
 	log << "\tEnd spawn_wall" << endl;
